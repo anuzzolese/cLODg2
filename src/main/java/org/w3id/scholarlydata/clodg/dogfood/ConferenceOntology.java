40175334a92fb0cded7df264dba1bbf74f9449ac
anuzzolese <andrea.nuzzolese@gmail.com>
2016-10-19 05:35:30 +0200

    Fix NS.

--- src/main/java/org/w3id/scholarlydata/clodg/dogfood/ConferenceOntology.java
+++ src/main/java/org/w3id/scholarlydata/clodg/dogfood/ConferenceOntology.java
@@ -31,163 +31,163 @@ public class ConferenceOntology {
     /** <p>The namespace of the vocabulary as a resource</p> */
     public static final Resource NAMESPACE = m_model.createResource( NS );
     
-    public static final Property abstract_ = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#abstract" );
+    public static final Property abstract_ = m_model.createProperty( NS + "abstract" );
     
-    public static final Property description = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#description" );
+    public static final Property description = m_model.createProperty( NS + "description" );
     
-    public static final Property doi = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#doi" );
+    public static final Property doi = m_model.createProperty( NS + "doi" );
     
-    public static final Property duration = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#duration" );
+    public static final Property duration = m_model.createProperty( NS + "duration" );
     
-    public static final Property during = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#during" );
+    public static final Property during = m_model.createProperty( NS + "during" );
     
-    public static final Property endDate = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#endDate" );
+    public static final Property endDate = m_model.createProperty( NS + "endDate" );
     
-    public static final Property familyName = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#familyName" );
+    public static final Property familyName = m_model.createProperty( NS + "familyName" );
     
-    public static final Property givenName = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#givenName" );
+    public static final Property givenName = m_model.createProperty( NS + "givenName" );
     
-    public static final Property hasAddress = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasAddress" );
+    public static final Property hasAddress = m_model.createProperty( NS + "hasAddress" );
     
-    public static final Property hasAffiliation = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasAffiliation" );
+    public static final Property hasAffiliation = m_model.createProperty( NS + "hasAffiliation" );
     
-    public static final Property hasAuthorList = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasAuthorList" );
+    public static final Property hasAuthorList = m_model.createProperty( NS + "hasAuthorList" );
     
-    public static final Property hasContent = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasContent" );
+    public static final Property hasContent = m_model.createProperty( NS + "hasContent" );
     
-    public static final Property hasFirstItem = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasFirstItem" );
+    public static final Property hasFirstItem = m_model.createProperty( NS + "hasFirstItem" );
     
-    public static final Property hasItem = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasItem" );
+    public static final Property hasItem = m_model.createProperty( NS + "hasItem" );
     
-    public static final Property hasLastItem = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasLastItem" );
+    public static final Property hasLastItem = m_model.createProperty( NS + "hasLastItem" );
     
-    public static final Property hasLocation = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasLocation" );
+    public static final Property hasLocation = m_model.createProperty( NS + "hasLocation" );
     
-    public static final Property relatesToEvent = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#relatesToEvent" );
+    public static final Property relatesToEvent = m_model.createProperty( NS + "relatesToEvent" );
     
-    public static final Property isEventRelatedTo = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isEventRelatedTo" );
+    public static final Property isEventRelatedTo = m_model.createProperty( NS + "isEventRelatedTo" );
     
-    public static final Property hasPart = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasPart" );
+    public static final Property hasPart = m_model.createProperty( NS + "hasPart" );
     
-    public static final Property hasSite = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasSite" );
+    public static final Property hasSite = m_model.createProperty( NS + "hasSite" );
     
-    public static final Property hasSubEvent = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasSubEvent" );
+    public static final Property hasSubEvent = m_model.createProperty( NS + "hasSubEvent" );
     
-    public static final Property hasTopic = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#hasTopic" );
+    public static final Property hasTopic = m_model.createProperty( NS + "hasTopic" );
     
-    public static final Property holdsRole = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#holdsRole" );
+    public static final Property holdsRole = m_model.createProperty( NS + "holdsRole" );
     
-    public static final Property inAffiliationDuringEvent = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#inAffiliationDuringEvent" );
+    public static final Property inAffiliationDuringEvent = m_model.createProperty( NS + "inAffiliationDuringEvent" );
     
-    public static final Property isAddressOf = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isAddressOf" );
+    public static final Property isAddressOf = m_model.createProperty( NS + "isAddressOf" );
     
-    public static final Property isAffiliationOf = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isAffiliationOf" );
+    public static final Property isAffiliationOf = m_model.createProperty( NS + "isAffiliationOf" );
     
-    public static final Property isDocumentRelatedTo = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isDocumentRelatedTo" );
+    public static final Property isDocumentRelatedTo = m_model.createProperty( NS + "isDocumentRelatedTo" );
     
-    public static final Property isFirstItemOf = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isFirstItemOf" );
+    public static final Property isFirstItemOf = m_model.createProperty( NS + "isFirstItemOf" );
     
-    public static final Property isHeldBy = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isHeldBy" );
+    public static final Property isHeldBy = m_model.createProperty( NS + "isHeldBy" );
     
-    public static final Property isItemOf = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isItemOf" );
+    public static final Property isItemOf = m_model.createProperty( NS + "isItemOf" );
     
-    public static final Property isLastItemOf = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isLastItemOf" );
+    public static final Property isLastItemOf = m_model.createProperty( NS + "isLastItemOf" );
     
-    public static final Property isLocationFor = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isLocationFor" );
+    public static final Property isLocationFor = m_model.createProperty( NS + "isLocationFor" );
     
-    public static final Property isPartOf = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isPartOf" );
+    public static final Property isPartOf = m_model.createProperty( NS + "isPartOf" );
     
-    public static final Property isSiteOf = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isSiteOf" );
+    public static final Property isSiteOf = m_model.createProperty( NS + "isSiteOf" );
     
-    public static final Property isSubEventOf = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isSubEventOf" );
+    public static final Property isSubEventOf = m_model.createProperty( NS + "isSubEventOf" );
     
-    public static final Property isTopicOf = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#isTopicOf" );
+    public static final Property isTopicOf = m_model.createProperty( NS + "isTopicOf" );
     
-    public static final Property keyword = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#keyword" );
+    public static final Property keyword = m_model.createProperty( NS + "keyword" );
     
-    public static final Property name = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#name" );
+    public static final Property name = m_model.createProperty( NS + "name" );
     
-    public static final Property next = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#next" );
+    public static final Property next = m_model.createProperty( NS + "next" );
     
-    public static final Property previous = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#previous" );
+    public static final Property previous = m_model.createProperty( NS + "previous" );
     
-    public static final Property relatesToDocument = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#relatesToDocument" );
+    public static final Property relatesToDocument = m_model.createProperty( NS + "relatesToDocument" );
     
-    public static final Property startDate = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#startDate" );
+    public static final Property startDate = m_model.createProperty( NS + "startDate" );
     
-    public static final Property title = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#title" );
+    public static final Property title = m_model.createProperty( NS + "title" );
     
-    public static final Property withOrganisation = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#withOrganisation" );
+    public static final Property withOrganisation = m_model.createProperty( NS + "withOrganisation" );
     
-    public static final Property withRole = m_model.createProperty( "http://www.scholarlydata.org/ontology/conference-ontology.owl#withRole" );
+    public static final Property withRole = m_model.createProperty( NS + "withRole" );
     
-    public static final Resource Abstract = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Abstract" );
+    public static final Resource Abstract = m_model.createResource( NS + "Abstract" );
     
-    public static final Resource AcademicEvent = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#AcademicEvent" );
+    public static final Resource AcademicEvent = m_model.createResource( NS + "AcademicEvent" );
     
-    public static final Resource Address = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Address" );
+    public static final Resource Address = m_model.createResource( NS + "Address" );
     
-    public static final Resource AffiliationDuringEvent = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#AffiliationDuringEvent" );
+    public static final Resource AffiliationDuringEvent = m_model.createResource( NS + "AffiliationDuringEvent" );
     
-    public static final Resource AffiliationRole = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#AffiliationRole" );
+    public static final Resource AffiliationRole = m_model.createResource( NS + "AffiliationRole" );
     
-    public static final Resource Agent = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Agent" );
+    public static final Resource Agent = m_model.createResource( NS + "Agent" );
     
-    public static final Resource Break = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Break" );
+    public static final Resource Break = m_model.createResource( NS + "Break" );
     
-    public static final Resource Conference = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Conference" );
+    public static final Resource Conference = m_model.createResource( NS + "Conference" );
     
-    public static final Resource Document = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Document" );
+    public static final Resource Document = m_model.createResource( NS + "Document" );
     
-    public static final Resource InProceedings = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#InProceedings" );
+    public static final Resource InProceedings = m_model.createResource( NS + "InProceedings" );
     
-    public static final Resource List = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#List" );
+    public static final Resource List = m_model.createResource( NS + "List" );
     
-    public static final Resource ListItem = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#ListItem" );
+    public static final Resource ListItem = m_model.createResource( NS + "ListItem" );
     
-    public static final Resource Meal = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Meal" );
+    public static final Resource Meal = m_model.createResource( NS + "Meal" );
     
-    public static final Resource NonAcademicEvent = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#NonAcademicEvent" );
+    public static final Resource NonAcademicEvent = m_model.createResource( NS + "NonAcademicEvent" );
     
-    public static final Resource Organisation = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Organisation" );
+    public static final Resource Organisation = m_model.createResource( NS + "Organisation" );
     
-    public static final Resource OrganisationalRoleDuringEvent = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#OrganisationalRoleDuringEvent" );
+    public static final Resource OrganisationalRoleDuringEvent = m_model.createResource( NS + "OrganisationalRoleDuringEvent" );
     
-    public static final Resource OrganisedEvent = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#OrganisedEvent" );
+    public static final Resource OrganisedEvent = m_model.createResource( NS + "OrganisedEvent" );
     
-    public static final Resource OrganisingRole = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#OrganisingRole" );
+    public static final Resource OrganisingRole = m_model.createResource( NS + "OrganisingRole" );
     
-    public static final Resource Panel = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Panel" );
+    public static final Resource Panel = m_model.createResource( NS + "Panel" );
     
-    public static final Resource Person = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Person" );
+    public static final Resource Person = m_model.createResource( NS + "Person" );
     
-    public static final Resource Proceedings = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Proceedings" );
+    public static final Resource Proceedings = m_model.createResource( NS + "Proceedings" );
     
-    public static final Resource PublishingRole = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#PublishingRole" );
+    public static final Resource PublishingRole = m_model.createResource( NS + "PublishingRole" );
     
-    public static final Resource PublishingRoleDuringEvent = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#PublishingRoleDuringEvent" );
+    public static final Resource PublishingRoleDuringEvent = m_model.createResource( NS + "PublishingRoleDuringEvent" );
     
-    public static final Resource Role = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Role" );
+    public static final Resource Role = m_model.createResource( NS + "Role" );
     
-    public static final Resource RoleDuringEvent = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#RoleDuringEvent" );
+    public static final Resource RoleDuringEvent = m_model.createResource( NS + "RoleDuringEvent" );
     
-    public static final Resource Session = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Session" );
+    public static final Resource Session = m_model.createResource( NS + "Session" );
     
-    public static final Resource Site = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Site" );
+    public static final Resource Site = m_model.createResource( NS + "Site" );
     
-    public static final Resource SocialEvent = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#SocialEvent" );
+    public static final Resource SocialEvent = m_model.createResource( NS + "SocialEvent" );
     
-    public static final Resource Talk = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Talk" );
+    public static final Resource Talk = m_model.createResource( NS + "Talk" );
     
-    public static final Resource TimeIndexedSituation = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#TimeIndexedSituation" );
+    public static final Resource TimeIndexedSituation = m_model.createResource( NS + "TimeIndexedSituation" );
     
-    public static final Resource Topic = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Topic" );
+    public static final Resource Topic = m_model.createResource( NS + "Topic" );
     
-    public static final Resource Track = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Track" );
+    public static final Resource Track = m_model.createResource( NS + "Track" );
     
-    public static final Resource Tutorial = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Tutorial" );
+    public static final Resource Tutorial = m_model.createResource( NS + "Tutorial" );
     
-    public static final Resource Workshop = m_model.createResource( "http://www.scholarlydata.org/ontology/conference-ontology.owl#Workshop" );
+    public static final Resource Workshop = m_model.createResource( NS + "Workshop" );
     
     public static final Resource author = m_model.createResource( "http://www.scholarlydata.org/resource/author" );
     
